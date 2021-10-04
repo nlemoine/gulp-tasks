644c7ebfcfe62bc2333ba244a0efb9034ab9c76b
Nicolas Lemoine <nico.lemoine@gmail.com>
2021-10-04 17:41:24 +0200

    switch to ESM

--- tasks/images.js
+++ tasks/images.js
@@ -21,42 +21,42 @@ export default (config) => {
 
   let defaultPlugins = [
     // GIF
-    // imagemin.gifsicle(),
+    imagemin.gifsicle(),
     // JPEG
-    // imagemin.mozjpeg({
-    //   quality: 100,
-    // }),
+    imagemin.mozjpeg({
+      quality: 100,
+    }),
     // PNG
     pngquant({
       quality: [0.5, 0.5],
     }),
     // SVG
-    // imagemin.svgo({
-    //   plugins: [
-    //     { cleanupAttrs: true },
-    //     { removeDoctype: true },
-    //     { removeXMLProcInst: true },
-    //     { removeComments: true },
-    //     { removeMetadata: true },
-    //     { removeUselessDefs: true },
-    //     { removeEditorsNSData: true },
-    //     { removeEmptyAttrs: true },
-    //     { removeHiddenElems: false },
-    //     { removeEmptyText: true },
-    //     { removeEmptyContainers: true },
-    //     { cleanupEnableBackground: true },
-    //     { removeViewBox: true },
-    //     { cleanupIDs: false },
-    //     { convertStyleToAttrs: true },
-    //     { removeViewBox: false },
-    //     {
-    //       cleanupIDs: {
-    //         remove: true,
-    //         minify: false,
-    //       },
-    //     },
-    //   ],
-    // }),
+    imagemin.svgo({
+      plugins: [
+        { cleanupAttrs: true },
+        { removeDoctype: true },
+        { removeXMLProcInst: true },
+        { removeComments: true },
+        { removeMetadata: true },
+        { removeUselessDefs: true },
+        { removeEditorsNSData: true },
+        { removeEmptyAttrs: true },
+        { removeHiddenElems: false },
+        { removeEmptyText: true },
+        { removeEmptyContainers: true },
+        { cleanupEnableBackground: true },
+        { removeViewBox: true },
+        { cleanupIDs: false },
+        { convertStyleToAttrs: true },
+        { removeViewBox: false },
+        {
+          cleanupIDs: {
+            remove: true,
+            minify: false,
+          },
+        },
+      ],
+    }),
   ];
 
   if (config.hasOwnProperty('plugins')) {
