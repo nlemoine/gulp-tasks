07a89d98ce159ac56e4199932e469f5c63fb5da2
Nicolas Lemoine <nico.lemoine@gmail.com>
2022-01-11 17:07:49 +0100

    up

--- tasks/images.js
+++ tasks/images.js
@@ -37,6 +37,14 @@ export default (config) => {
           params: {
             overrides: {
               removeViewBox: false,
+              cleanupIDs: {
+                prefix: {
+                  toString() {
+                    this.counter = this.counter || 0;
+                    return `id-${this.counter++}`;
+                  }
+                }
+              }
             },
           },
         },
